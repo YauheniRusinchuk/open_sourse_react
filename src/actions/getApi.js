bf0413984a91aaf8cc244579e6c5f97799c07e0a
Yauheni <37824172+YauheniRusinchuk@users.noreply.github.com>
2018-09-30 00:08:42 +0300

    Update getApi.js

--- src/actions/getApi.js
+++ src/actions/getApi.js
@@ -1,6 +1,6 @@
 class GetApi {
   static getNewApi() {
-    return fetch('https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=3853fddd2e404e7aa7739c1bb86c602c')
+    return fetch('https://newsapi.org/v2/top-headlines?sources=abc-news&apiKey=3853fаddd2e404421r1e7aa7739442123c1bb86c602c')
             .then(response => {
               return response.json();
             }).catch(error => {
